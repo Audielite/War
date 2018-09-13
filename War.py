dce4e66809ef0a70ffdad651e64a72a44016dd7b
RHN <stuff@stuff.com>
2018-09-12 19:37:13 -0500

    started CheckForTie method. Now Displays both cards before declaring winner

--- War.py
+++ War.py
@@ -3,9 +3,12 @@ import random
 
 class Cards:
     def __init__(self):
-        self.suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
-        self.values = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6', '5', '4', '3', '2']
-        self.actualCards = list(itertools.product(self.suits, self.values))
+        def __init__(self):
+            self.suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
+            self.values = {'Ace': 14, 'King': 13, 'Queen': 12, 'Jack': 11, '10': 10, '9': 9, '8': 8, '7': 7, '6': 6,
+                           '5': 5,
+                           '4': 4, '3': 3, '2': 2}
+            self.actualCards = list(itertools.product(self.suits, self.values))
 
     def GetRandomCard(self):
         randomDeal = random.randint(0, 51)
@@ -25,9 +28,9 @@ class War(Game):
     def __init__(self, numberOfPlayers):
         self.numberOfPlayers = numberOfPlayers
         self.players = []
+        self.deckOfCards = Cards()
 
     def StartGame(self):
-        deckOfCards = Cards()
         for playerID in range(0, self.numberOfPlayers):
             cardForPlayer = deckOfCards.GetRandomCard() #Deal card
             newPlayer = Player(playerID, cardForPlayer) #Save player and card
@@ -35,14 +38,23 @@ class War(Game):
         self.DecideWinner()
 
     def DecideWinner(self):
-        winningID = self.players [0] #Choose potential winner
-        for playerID in self.players:
-            if(playerID.cardForPlayer[1] > winningID.cardForPlayer[1]):
-                winningID = playerID #find highest card
-        print ("The winner is player " + str(winningID.playerID)+ "!")
-        print ("The winning card is the " + str(winningID.cardForPlayer[1]) + " of " + str(winningID.cardForPlayer[0])+ ".")
+        winningID = self.players[0]  # Choose potential winner
+        print("Player 0 Card: " + str(winningID.cardForPlayer))
+        print("Player 1 Card: " + str(self.players[1].cardForPlayer))
+
+        if self.players[1].cardForPlayer[1] > winningID.cardForPlayer[1]:
+            # TODO For some reason 10 is lower than everything.
+            winningID = self.players[1]  # find highest card
+        print("The winner is player " + str(winningID.playerID) + "!")
+        print("The winning card is the " + str(winningID.cardForPlayer[1]) + " of " + str(winningID.cardForPlayer[0]) + ".")
+
+    def CheckForTie(self, player0, player1):
+        if player0.cardForPlayer[1] == player1.cardForPlayer[1]:
+            while player0.cardForPlayer[1] == player1.cardForPlayer[1]:
+                # TODO deal card to player 0 and 1 then print.
+                return
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     newGame = War(2)
     newGame.StartGame()

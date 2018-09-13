bff884ed32275fb59687d9fbe7853bc300cf911f
RHN <stuff@stuff.com>
2018-09-12 20:04:22 -0500

    completed function to check for ties which then just pulls another card from the deck. Also .pop() card from card array when it is dealt to a player.

--- War.py
+++ War.py
@@ -3,16 +3,15 @@ import random
 
 class Cards:
     def __init__(self):
-        def __init__(self):
-            self.suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
-            self.values = {'Ace': 14, 'King': 13, 'Queen': 12, 'Jack': 11, '10': 10, '9': 9, '8': 8, '7': 7, '6': 6,
-                           '5': 5,
-                           '4': 4, '3': 3, '2': 2}
-            self.actualCards = list(itertools.product(self.suits, self.values))
+        self.suits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
+        self.values = {'Ace': 14, 'King': 13, 'Queen': 12, 'Jack': 11, '10': 10, '9': 9, '8': 8, '7': 7, '6': 6,
+                           '5': 5,'4': 4, '3': 3, '2': 2}
+        self.actualCards = list(itertools.product(self.suits, self.values))
 
     def GetRandomCard(self):
-        randomDeal = random.randint(0, 51)
+        randomDeal = random.randint(0, len(self.actualCards) - 1)
         cardDealt = self.actualCards[randomDeal] #Generate random card
+        self.actualCards.pop(randomDeal)  # Remove card from deck
         return (cardDealt)
 
 class Player:
@@ -32,16 +31,16 @@ class War(Game):
 
     def StartGame(self):
         for playerID in range(0, self.numberOfPlayers):
-            cardForPlayer = deckOfCards.GetRandomCard() #Deal card
+            cardForPlayer = self.deckOfCards.GetRandomCard() #Deal card
             newPlayer = Player(playerID, cardForPlayer) #Save player and card
             self.players.append(newPlayer)
         self.DecideWinner()
 
     def DecideWinner(self):
         winningID = self.players[0]  # Choose potential winner
-        print("Player 0 Card: " + str(winningID.cardForPlayer))
-        print("Player 1 Card: " + str(self.players[1].cardForPlayer))
-
+        print("Player 0 Card: " + str(winningID.cardForPlayer[1]) + " of " + str(winningID.cardForPlayer[0]))
+        print("Player 1 Card: " + str(self.players[1].cardForPlayer[1]) + " of " + str(self.players[1].cardForPlayer[0]))
+        self.CheckForTie(winningID, self.players[1])
         if self.players[1].cardForPlayer[1] > winningID.cardForPlayer[1]:
             # TODO For some reason 10 is lower than everything.
             winningID = self.players[1]  # find highest card
@@ -51,8 +50,11 @@ class War(Game):
     def CheckForTie(self, player0, player1):
         if player0.cardForPlayer[1] == player1.cardForPlayer[1]:
             while player0.cardForPlayer[1] == player1.cardForPlayer[1]:
-                # TODO deal card to player 0 and 1 then print.
-                return
+                player0.cardForPlayer = self.deckOfCards.GetRandomCard()
+                player1.cardForPlayer = self.deckOfCards.GetRandomCard()
+
+                print("Player 0 Card: " + str(player0.cardForPlayer[1]) + " of " + str(player0.cardForPlayer[0]))
+                print("Player 1 Card: " + str(player1.cardForPlayer[1]) + " of " + str(player1.cardForPlayer[0]))
 
 
 if __name__ == "__main__":
